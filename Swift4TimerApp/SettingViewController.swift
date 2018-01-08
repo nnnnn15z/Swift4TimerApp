ea1ecc2c7c8466a909850255ced7d56616c6e9e6
ryo.takahashi <b152029@kca.ac.jp>
2018-01-08 23:32:14 +0900

    req.45 設定画面のViewControllerを追加しよう セクション5、レクチャー45 ダッシュボードへ移動

---
+++ Swift4TimerApp/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  Swift4TimerApp
+//
+//  Created by AdminAir on 2018/01/08.
+//  Copyright © 2018年 ryotakahashi. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
